a94f866d1f49b74fe2e7ef2e85570448146fec8e
ShubhamInnk <shubham@btr-india.co.in>
2023-08-29 19:41:51 +0530

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-function App() {
-  return <div className="App">hello</div>;
-}
+const App = () => {
+  return (
+    <div className="App">
+      <div className="btn-wrapper">
+        <button>Add Shape</button>
+        <button>Print JSON</button>
+      </div>
+      <div className="canvas"></div>
+    </div>
+  );
+};
 
 export default App;
